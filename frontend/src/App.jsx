56c3019661c7794b582008add75d65b81998c536
Ku priyanshu <113988502+KuPriyanshu@users.noreply.github.com>
2024-12-26 01:00:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
-const server = "https://module-2-major-p.onrender.com/";
+const server = "https://major-project-module-2.onrender.com/";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
